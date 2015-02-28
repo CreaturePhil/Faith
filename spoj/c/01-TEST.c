eef50143af24869fa78a00df044be550d82a4f26
Phil <birkal@outlook.com>
2015-02-28 00:32:12 -0800

    Add c and update gitignore

---
+++ spoj/c/01-TEST.c
@@ -0,0 +1,42 @@
+/**
+ * http://www.spoj.com/problems/TEST/
+ * 
+ * 1. Life, the Universe, and Everything
+ * 
+ * Problem code: TEST
+ * 
+ * Your program is to use the brute-force approach in order to find the Answer 
+ * to Life, the Universe, and Everything. More precisely... rewrite small numbers 
+ * from input to output. Stop processing input after reading in the number 42. 
+ * All numbers at input are integers of one or two digits.
+ * 
+ * Example
+ * 
+ * Input:
+ * 1
+ * 2
+ * 88
+ * 42
+ * 99
+ * 
+ * Output:
+ * 1
+ * 2
+ * 88
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  int num;
+  scanf("%d", &num);
+
+  while (num != 42)
+  {
+    printf("%d\n", num);
+    scanf("%d", &num);
+  }
+
+  return 0;
+}
